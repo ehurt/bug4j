706eac252bbc0f43c94e7b3d8f0f583788bab4b3
cdandoy <cdandoy@gmail.com>
2012-06-03 22:02:05 +0000

    merged the ExtensionManager into the ExtensionService

--- bug4jd/grails-app/services/org/bug4j/ExtensionService.groovy
+++ bug4jd/grails-app/services/org/bug4j/ExtensionService.groovy
@@ -15,38 +15,175 @@
  */
 package org.bug4j
 
-import org.bug4j.server.extension.ExtensionManager
+import groovy.io.FileType
+import org.apache.commons.io.FilenameUtils
+import org.apache.commons.io.filefilter.WildcardFileFilter
+import org.codehaus.groovy.runtime.InvokerHelper
+
+import java.util.jar.JarFile
 
 class ExtensionService {
-    private ExtensionManager _extensionManager
+    private final Map<String, String> _actions = [:]
+    private final Map<String, List<String>> _events = [:]
+    private final Binding _binding = new Binding()
+    private GroovyScriptEngine _groovyScriptEngine
 
     public void init(File extensionRoot) {
-        if (_extensionManager == null) {
-            _extensionManager = new ExtensionManager(extensionRoot);
+        if (extensionRoot.isDirectory()) {
+            final File[] jarFiles = extensionRoot.listFiles((FileFilter) new WildcardFileFilter("*.jar"))
+            loadJarExtensions(jarFiles)
+            loadDirectoryExtensions(extensionRoot)
+            println "Actions: ${_actions}"
+            _events.each {println "Event ${it.key}=${it.value}"}
+
+            def fileNames = jarFiles*.getAbsoluteFile()
+            fileNames += extensionRoot
+            final URL[] urls = fileNames*.toURI()*.toURL()
+            _groovyScriptEngine = new GroovyScriptEngine(urls)
         }
     }
 
     public void whenBug(Bug bug) {
-        _extensionManager.whenEvent('events/whenBug', [
+        whenEvent('events/whenBug', [
                 bug: bug,
         ])
     }
 
     public void whenHit(Hit hit) {
-        _extensionManager.whenEvent('events/whenHit', [
+        whenEvent('events/whenHit', [
                 hit: hit,
         ])
     }
 
     public void reportBug(Bug bug) {
-        _extensionManager.whenAction('actions/reportBug', [
+        whenAction('actions/reportBug', [
                 bug: bug,
         ])
     }
 
     public String getBugURL(Bug bug) {
-        _extensionManager.whenAction('action/getBug', [
+        whenAction('action/getBug', [
                 bug: bug,
         ])
+    }
+
+    private void whenEvent(String eventName, Map<String, Object> params) {
+        params.each {
+            _binding.setVariable(it.key, it.value)
+        }
+        final List<String> scriptNames = _events.get(eventName)
+        scriptNames.each {
+            runScriptOrClass(it)
+        }
+    }
+
+    private Object whenAction(String actionName, Map<String, Object> params) {
+        Object ret = null
+        final scriptName = _actions.get(actionName)
+        if (scriptName) {
+            params.each {
+                _binding.setVariable(it.key, it.value)
+            }
+            ret = runScriptOrClass(scriptName)
+        }
+        return ret
+    }
+
+    private def runScriptOrClass(String name) {
+        if (_groovyScriptEngine) { // will be null if the extension directory did not exist
+            try {
+                if (name.endsWith('.groovy')) {
+                    return _groovyScriptEngine.run(name, _binding)
+                } else {
+                    final classLoader = _groovyScriptEngine.getGroovyClassLoader()
+                    final Class clazz = classLoader.loadClass(name)
+                    final script = InvokerHelper.createScript(clazz, _binding)
+                    return script.run()
+                }
+            } catch (Exception e) {
+                LOGGER.error("Exception while running " + name, e)
+            }
+        }
+    }
+
+    private void loadDirectoryExtensions(File extensionRoot) {
+        final baseRootName = extensionRoot.getPath()
+        extensionRoot.eachFileRecurse(FileType.FILES) {
+            final path = it.getPath()
+            path = path.substring(baseRootName.length())
+            registerExtention(path)
+        }
+    }
+
+    private List<String> loadJarExtensions(File[] jarFiles) {
+        final List<String> ret = []
+        for (File jarFile : jarFiles) {
+            loadJarExtensions(jarFile)
+        }
+        return ret
+    }
+
+    private List<String> loadJarExtensions(File file) {
+        final jarFile = new JarFile(file)
+        final entries = jarFile.entries()
+        while (entries.hasMoreElements()) {
+            final entry = entries.nextElement()
+            if (!entry.isDirectory()) {
+                final name = entry.getName()
+                registerExtention(name)
+            }
+        }
+        return []
+    }
+
+    /**
+     * @param name actions/reportHit.groovy or actions/reportHit.class
+     */
+    @SuppressWarnings("GroovyEmptyStatementBody")
+    private void registerExtention(String name) {
+        if (name.endsWith('.groovy') || name.endsWith('.class')) {
+            name = name.replace('\\', '/')
+            if (name.startsWith('/')) {
+                name = name.substring(1)
+            }
+            if (name.startsWith('events/')) {
+                registerEvent(name)
+            } else if (name.startsWith('actions/')) {
+                registerAction(name)
+            }
+        }
+    }
+
+    /**
+     * @param name events/whenHit/printHit.groovy or events/whenHit/printHit.class
+     */
+    private void registerEvent(String name) {
+        final eventName = FilenameUtils.getPathNoEndSeparator(name)
+        final events = _events.get(eventName)
+        if (!events) {
+            events = []
+            _events.put(eventName, events)
+        }
+        final registeredName = toScriptOrClassName(name)
+        events.add(registeredName)
+    }
+
+    /**
+     * @param name actions/reportBug.groovy or actions/reportBug.class
+     */
+    private void registerAction(String name) {
+        final eventName = FilenameUtils.removeExtension(name)
+        final scriptOrClassName = toScriptOrClassName(name)
+        _actions.put(eventName, scriptOrClassName)
+    }
+
+    private static String toScriptOrClassName(String fileName) {
+        if (fileName.endsWith('.groovy')) {
+            return fileName
+        } else {
+            fileName = FilenameUtils.removeExtension(fileName)
+            fileName = fileName.replace('/', '.')
+            return fileName
+        }
     }
 }
