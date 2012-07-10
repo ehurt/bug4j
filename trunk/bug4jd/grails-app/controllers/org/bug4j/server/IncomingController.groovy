afe11eca095223cf07172daa8e74a9ec953f7482
cdandoy@gmail.com <cdandoy@gmail.com@e2f1cc9d-c2ab-5037-24a5-639e5d755c76>
2012-07-10 13:43:43 +0000

    Adding traces to fix a mysterious bug: java.lang.NumberFormatException at IncomingController.groovy:90

--- trunk/bug4jd/grails-app/controllers/org/bug4j/server/IncomingController.groovy
+++ trunk/bug4jd/grails-app/controllers/org/bug4j/server/IncomingController.groovy
@@ -15,9 +15,6 @@
  */
 package org.bug4j.server
 
-
-
-
 import org.bug4j.ClientSession
 
 class IncomingController {
@@ -25,38 +22,38 @@ class IncomingController {
     def bugService
 
     def createSession() {
+        final String remoteAddr = request.remoteAddr
         try {
             final String appCode = params.a
             final String appVersion = params.v
             final Long buildDateMillis = params.d as Long
             final String devBuild = params.dev
             final Integer buildNumber = params.n as Integer
-            final String remoteAddr = request.remoteAddr
 
             try {
                 ClientSession clientSession = bugService.createSession(appCode, appVersion, buildDateMillis, devBuild, buildNumber, remoteAddr)
                 render(text: clientSession.id, contentType: 'text/plain')
-                log.info "Created a session"
+                log.info "Created session ${clientSession.id} for ${remoteAddr}"
             } catch (IllegalArgumentException e) {
-                log.error("Failed to create a session", e)
+                log.error("Failed to create a session for ${remoteAddr}", e)
                 response.sendError(400, e.getMessage())
             } catch (IllegalStateException e) {
-                log.error("Failed to create a session", e)
+                log.error("Failed to create a session for ${remoteAddr}", e)
                 response.sendError(500, e.getMessage())
             }
         } catch (Exception e) {
-            log.error("Failed to create a session", e)
+            log.error("Failed to create a session for ${remoteAddr}", e)
         }
     }
 
     def check() {
+        final String remoteAddr = request.remoteAddr
         try {
             final String sessionId = params.e
             final String appCode = params.a
             final String message = params.m
             final String user = params.u
             final String hash = params.h
-            final String remoteAddr = request.remoteAddr
 
             try {
                 if (bugService.isNewBug(sessionId, appCode, message, user, remoteAddr, hash)) {
@@ -66,32 +63,38 @@ class IncomingController {
                     render(text: 'Old', contentType: 'text/plain')
                 }
             } catch (IllegalArgumentException e) {
-                log.error("Failed to check", e)
+                log.error("Failed to check for ${remoteAddr}", e)
                 response.sendError(400, e.getMessage())
             } catch (IllegalStateException e) {
-                log.error("Failed to check", e)
+                log.error("Failed to check  for ${remoteAddr}", e)
                 response.sendError(500, e.getMessage())
             }
         } catch (Exception e) {
-            log.error("Failed to check", e)
+            log.error("Failed to check for ${remoteAddr}", e)
         }
     }
 
     def bug() {
+        final String remoteAddr = request.remoteAddr
+
         try {
-            final String sessionId = params.e
+            final String sessionIdValue = params.e
             final String appCode = params.a
             final String message = params.m
             final long dateReported = System.currentTimeMillis()
             final String user = params.u
             final String stackText = params.s
-            final String remoteAddr = request.remoteAddr
+            if (sessionIdValue) {
+                final long sessionId = Long.parseLong(sessionIdValue)
 
-            def bugId = bugService.reportBug(Long.parseLong(sessionId), appCode, message, dateReported, user, remoteAddr, stackText)
-            log.info "Reported bug ${bugId}"
-            render(text: bugId, contentType: 'text/plain')
+                def bugId = bugService.reportBug(sessionId, appCode, message, dateReported, user, remoteAddr, stackText)
+                log.info "Reported bug ${bugId} for ${remoteAddr}"
+                render(text: bugId, contentType: 'text/plain')
+            } else {
+                log.error("Missing session ID in IncomingController.bug for ${remoteAddr}")
+            }
         } catch (Throwable e) {
-            log.error("Failed to create a session", e)
+            log.error("Failed to report a bug for ${remoteAddr}", e)
         }
     }
 }
